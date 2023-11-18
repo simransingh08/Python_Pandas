d8449076658811ed2950e8616caf7c4f815e2188
simransin <simransingh9876rajgir@gmail.com>
2023-11-18 22:57:02 +0530

    pandas

--- 1_Pandas.py
+++ 1_Pandas.py
@@ -6,5 +6,5 @@ lst = [['Jeet', 25], ['is', 30],
        ['Rajveer', 26], ['Simran', 22]]  
   
 # creating df object with columns specified     
-df = pd.DataFrame(lst, columns =['Tag', 'number'])  
-print(df ) 
+df = pd.DataFrame(lst, columns =['Tag', 'number'])  ;
+print(df ) ;
