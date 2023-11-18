fa2f2e664313cce6fd0e1648875378890efc7d31
simransin <simransingh9876rajgir@gmail.com>
2023-11-18 22:55:23 +0530

    Pandas

---
+++ 1_Pandas.py
@@ -0,0 +1,10 @@
+# import pandas as pd  
+import pandas as pd   
+      
+# List1   
+lst = [['Jeet', 25], ['is', 30],  
+       ['Rajveer', 26], ['Simran', 22]]  
+  
+# creating df object with columns specified     
+df = pd.DataFrame(lst, columns =['Tag', 'number'])  
+print(df ) 
