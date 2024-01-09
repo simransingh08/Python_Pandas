bdb197f78e825a742b826782b726402c9dd1ff6f
simransin <simransingh9876rajgir@gmail.com>
2024-01-09 22:25:58 +0530

    dataframe_index

---
+++ 6_dataframe_index.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+data = {'Name': ['John', 'Alice', 'Bob'],
+        'Age': [25, 28, 32],
+        'City': ['New York', 'London', 'Paris']}
+
+df = pd.DataFrame(data)
+print(df)
